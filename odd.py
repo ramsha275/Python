e6a38a1236491c0093a9f3d12e1e82f554a8abd4
ramsha275 <33019590+ramsha275@users.noreply.github.com>
2017-11-04 17:02:00 +0500

    Add files via upload

---
+++ odd.py
@@ -0,0 +1,5 @@
+def odd_list(a):
+    for num in range(1,a+1):
+        if num%2!=0:
+            print(num)
+odd_list(11)
---
+++ prime.py
@@ -0,0 +1,8 @@
+max=int(input("Enter maximum limit: "))
+for a in range(2,max+1):
+    count=0
+    for b in range(2,a//2):
+        if(a%b==0):
+            count=count+1
+    if(count<=0):
+        print(a)
